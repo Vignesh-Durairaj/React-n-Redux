e78285af5efe5c5468cf3d7a375b67be0c40ec8a
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-11-14 13:10:54 +0800

    Added styles

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = React.createElement(
-    'hi', 
-    {id:'title', className: 'header'}, 
+let textStyle = {
+    color: 'darkgreen'
+  }
+
+const title = React.createElement('label', {}, 'React Essentials')
+
+const content = React.createElement(
+    'h1', 
+    {id:'title', className: 'header', style: textStyle}, 
     'Hello World'
 );
 
-ReactDOM.render(title, document.getElementById('title-component'));
+ReactDOM.render(title, document.getElementById('page-title'));
+ReactDOM.render(content, document.getElementById('title-component'));
