bc0afcd6088586188d15dd9be6be6aafa608cf36
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-11-19 12:39:25 +0800

    Descoping the imports and using class objects

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 
 let textStyle = {
     color: 'darkgreen'
-  }
+}
+
+let myData = {
+    total: 4,
+    country: 3, 
+    temples: 12
+}
 
 const title = React.createElement('label', {}, 'React Essentials')
 
@@ -23,7 +29,7 @@ const listContent = React.createElement(
     )
 );
 
-class MessageComponent extends React.Component {
+class MessageComponent extends Component {
     render() {
         console.log(this.props);
         return (
@@ -31,17 +37,18 @@ class MessageComponent extends React.Component {
                 <h3 style={{color:this.props.colour}}>
                     This is a custom component. {this.props.msg}. I hope you are {this.props.age} years old !
                 </h3>
+                And you have been to {this.props.countryData.country} many countries with a total of {this.props.countryData.total} and visited {this.props.countryData.temples} different temples.
             </div>
         )
     }
 }
 
-ReactDOM.render(
+render(
     <div style={textStyle}>
         <h1>Hello World</h1>
         <p>Let's see some list of items</p>
     </div>, 
     document.getElementById('page-title'));
 
-ReactDOM.render(<MessageComponent age={32} msg='Hello everyone' colour='green'/>, document.getElementById('custom-component'))
-ReactDOM.render(listContent, document.getElementById('title-component'));
+render(<MessageComponent age={32} msg='Hello everyone' colour='green' countryData = {myData}/>, document.getElementById('custom-component'))
+render(listContent, document.getElementById('title-component'));
