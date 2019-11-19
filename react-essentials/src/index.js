9a87eed6e504273bb8e5a4f4fa06aa27553961d8
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-11-19 12:55:43 +0800

    Adding components as functions

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -29,23 +29,29 @@ const listContent = React.createElement(
     )
 );
 
+const getAchievement = (countryData) => {
+    return countryData.country * 100 / countryData.target
+}
+
+const messageComponentGenerator = (propObject) => {
+    return (
+        <div>
+            <h3 style={{color:propObject.colour}}>
+                This is a custom component. {propObject.msg}. I hope you are {propObject.age} years old !
+            </h3>
+            And you have been to {propObject.countryData.country} many countries for a target of {propObject.countryData.target} and visited {propObject.countryData.temples} different temples.
+            <br />
+            Your target is achieved to {getAchievement(propObject.countryData)} %.
+        </div>
+    )
+}
+
 class MessageComponent extends Component {
-    getAchievement = () => {
-        return this.props.countryData.country * 100 / this.props.countryData.target
-    }
+    
 
     render() {
         console.log(this.props);
-        return (
-            <div>
-                <h3 style={{color:this.props.colour}}>
-                    This is a custom component. {this.props.msg}. I hope you are {this.props.age} years old !
-                </h3>
-                And you have been to {this.props.countryData.country} many countries for a target of {this.props.countryData.target} and visited {this.props.countryData.temples} different temples.
-                <br />
-                Your target is achieved to {this.getAchievement()} %.
-            </div>
-        )
+        return messageComponentGenerator(this.props);
     }
 }
 
