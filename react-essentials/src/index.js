1fe9d7c8940ec260e4fae3012bea6c396d845bd0
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-11-22 14:27:45 +0800

    Added country list

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 let countries = [
-    {"name": "Hong Kong", "city": "HK Island", "Currency": "Hong Kong Dollar"}, 
-    {"name": "Singapore", "city": "Changi", "Currency": "Singapore Dollar"}, 
-    {"name": "China", "city": "Shenzhen", "Currency": "Chinese Yuan"}, 
-    {"name": "China", "city": "Guangzhou", "Currency": "Chinese Yuan"}
+    {"name": "Hong Kong", "city": "HK Island", "currency": "Hong Kong Dollar"}, 
+    {"name": "Singapore", "city": "Changi", "currency": "Singapore Dollar"}, 
+    {"name": "China", "city": "Shenzhen", "currency": "Chinese Yuan"}, 
+    {"name": "China", "city": "Guangzhou", "currency": "Chinese Yuan"}
 ]
 
 let textStyle = {
@@ -50,7 +50,6 @@ const messageComponentGenerator = (propObject) => {
             <br />
             Your target is achieved to {getAchievement(propObject.countryData)} %.
             <hr />
-            {countryList(propObject.countries)}
         </div>
     )
 }
@@ -58,17 +57,23 @@ const messageComponentGenerator = (propObject) => {
 const Country = (prop) => {
     return (
         <section>
-            <h5>{prop.name}</h5>
+            <h3>{prop.name}</h3><hr />
+            <h5>{prop.city}</h5><br />
+            <h6>And used {prop.ccy}</h6>
         </section>
     )
 }
 
-const countryList = ({countryList}) =>  {
+const CountryListComponent = ({ctrys}) =>  {
     return (
         <div>
         <label>List of countries visited : </label>
         <br />
-            {countryList.map(ctry => <Country name={ctry.name} city={ctry.city} ccy={ctry.currency}/>)}
+            {ctrys.map(
+                ctry => <Country 
+                            name={ctry.name} 
+                            city={ctry.city} 
+                            ccy={ctry.currency}/>)}
         </div>
     )
 }
@@ -89,3 +94,4 @@ render(
 
 render(<MessageComponent age={32} msg='Hello everyone' colour='green' countryData = {myData} countries = {countries} />, document.getElementById('custom-component'))
 render(listContent, document.getElementById('title-component'));
+render(<CountryListComponent ctrys = {countries} />, document.getElementById('country-list'));
